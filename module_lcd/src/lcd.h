2f981372766f75450314aae8f98db4b9fb84a942
Andrew <andrew@andrew-Aspire-5920G.(none)>
2012-09-18 11:01:22 +0100

    updated some docs and removed duplicate app

--- module_lcd/src/lcd.h
+++ module_lcd/src/lcd.h
@@ -2,32 +2,36 @@
 #define _lcd_h_
 
 #include "lcd_defines.h"
-
-struct lcd_ports
-{
-  /* The clock line */
-  out port lcd_clk;
-
-  /* The LCD signal lines */
-  out port lcd_data_enabled;
-
-  /* 16 bit data port */
-  out port lcd_rgb;
-
-  /* The hsync line */
-  out buffered port:32 lcd_hsync;
-
-  /* The vsync line */
-  out buffered port:32 lcd_vsync;
-
-  /* Clock block used for LCD clock */
-  clock clk_lcd;
+/**
+ * The structure to represent LCD port configuration
+ */
+struct lcd_ports {
+  out port lcd_clk; 			/**< The clock line */
+  out port lcd_data_enabled; 		/**< The LCD signal lines */
+  out port lcd_rgb; 			/**< 16 bit data port */
+  out buffered port:32 lcd_hsync; 	/**< The hsync line */
+  out buffered port:32 lcd_vsync; 	/**< The vsync line */
+  clock clk_lcd; 			/**< Clock block used for LCD clock */
 };
 
-void lcd_server(chanend c_lcd, struct lcd_ports &ports);
+/** \brief The LCD server thread.
+* 
+* \param client The channel end connecting to the client.
+* \param ports The structure carrying the LCD port details. 
+*/
+void lcd_server(chanend client, struct lcd_ports &ports);
 
+/** \brief LCD init function. This sets the lcd into a state where it is ready to accept data.
+* 
+* \param c_lcd The channel end connecting to the lcd server.
+*/
 void lcd_init(chanend c_lcd);
 
+/** \brief LCD update function. This sends a buffer of data to the lcd server to to sent to the lcd.
+* 
+* \param c_lcd The channel end connecting to the lcd server.
+* \param buffer[] The data to to emitted to the lcd screen, stored in rgb565. Note, no array bounds checking is performed.
+*/
 static inline void lcd_update(chanend c_lcd, unsigned buffer[]){
 	unsigned buffer_pointer;
 	asm  ("mov %0, %1" : "=r"(buffer_pointer) : "r"(buffer));
